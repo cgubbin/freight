e4aa18f44a789246719592f46a3ec316a57d8590
cgubbin <chris.gubbin@googlemail.com>
2023-08-13 13:45:38 +0100

    added initial cli processing for stage1

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,35 @@
-#[cfg(not(stage1))]
-use freight::CrateType;
-#[cfg(not(stage1))]
-use freight::Edition;
-#[cfg(not(stage1))]
-use freight::Rustc;
-#[cfg(not(stage1))]
-use std::fs;
-#[cfg(not(stage1))]
-use std::path::PathBuf;
-#[cfg(not(stage1))]
-const BOOTSTRAP_STAGE1: &str = "bootstrap_stage1";
-
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>>{
     #[cfg(not(stage1))]
     {
+        use freight::CrateType;
+        use freight::Edition;
+        use freight::Rustc;
+        use std::fs;
+        use std::path::PathBuf;
+        const BOOTSTRAP_STAGE1: &str = "bootstrap_stage1";
+
         let target_dir = PathBuf::from("target");
         let bootstrap_dir = target_dir.join(BOOTSTRAP_STAGE1);
         Rustc::builder()
             .edition(Edition::E2021)
             .crate_type(CrateType::Lib)
-            .crate_name("freight".into())
+            .crate_name("freight")
             .out_dir(bootstrap_dir.clone())
             .lib_dir(bootstrap_dir.clone())
-            .cfg("stage1".into())
+            .cfg("stage1")
             .done()
             .run("src/lib.rs")?;
 
         Rustc::builder()
             .edition(Edition::E2021)
             .crate_type(CrateType::Bin)
-            .crate_name("freight_stage1".into())
+            .crate_name("freight_stage1")
             .out_dir(bootstrap_dir.clone())
             .lib_dir(bootstrap_dir)
-            .cfg("stage1".into())
+            .cfg("stage1")
+            .externs("freight")
             .done()
             .run("src/main.rs")?;
 
@@ -43,7 +38,24 @@ fn main() -> Result<(), Box<dyn Error>>{
     }
     #[cfg(stage1)]
     {
-        println!("successfully bootstrapped");
+        use std::env;
+        use std::process;
+
+        const HELP: &str = "\
+            Alternative for Cargo\n\n\
+            Usage: freight [COMMAND] [OPTIONS]\n\n\
+            Commands:\n    \
+                help    Print out this message
+        ";
+        let mut args = env::args().skip(1);
+        match args.next().as_ref().map(String::as_str) {
+            Some("help") => println!("{HELP}"),
+            _ => {
+                println!("unsupported command");
+                println!("{HELP}");
+                process::exit(1);
+            }
+        }
     }
     Ok(())
 }
