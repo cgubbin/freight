6dae5243610370a64c567f1b65f6f1e6e2930567
cgubbin <chris.gubbin@googlemail.com>
2023-09-29 13:53:01 +0100

    added test harness

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,29 @@ use std::env;
 use std::error::Error;
 use std::process;
 
-fn main() -> Result<(), Box<dyn Error>>{
+fn main() {
+    if let Err(err) = try_main() {
+        eprintln!("{}", err);
+        process::exit(2);
+    }
+}
+
+fn try_main() -> Result<(), Box<dyn Error>>{
     const HELP: &str = "\
         Alternative for Cargo\n\n\
         Usage: freight [COMMAND] [OPTIONS]\n\n\
         Commands:\n    \
             build    Build a Freight or Cargo project
+            test     Test a Freight or Cargo project
             help     Print out this message
     ";
     let mut args = env::args().skip(1);
     match args.next().as_ref().map(String::as_str) {
         Some("build") => freight::build()?,
+        Some("test") => {
+            freight::build_tests()?;
+            freight::run_tests()?;
+        }
         Some("help") => println!("{HELP}"),
         _ => {
             println!("unsupported command");
